fb9fd9734762ae9292758765949699580fbd43c4
RandyWei <weizhengheng@chinahrt.com>
2024-01-02 17:54:31 +0800

    关于页面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
 import Home from "./components/Home.vue";
 import Settings from "./components/Settings.vue";
 import HuaWei from "./components/HuaWei.vue";
+import About from "./components/About.vue";
 
 //TODO 需要监测设备连接状态来决定是否激活其他的Tab
 
@@ -13,9 +14,8 @@ import HuaWei from "./components/HuaWei.vue";
     <el-tabs type="border-card" tab-position="left" stretch class="w-full h-full">
         <el-tab-pane><template #label><span class="font-size-4">首页</span></template><Home></Home></el-tab-pane>
         <el-tab-pane label="华为"><template #label><span class="font-size-4">华为</span></template><HuaWei></HuaWei></el-tab-pane>
-        <el-tab-pane label="Config"><template #label><span class="font-size-4">Config</span></template>Config</el-tab-pane>
-        <el-tab-pane label="Role"><template #label><span class="font-size-4">Role</span></template>Role</el-tab-pane>
         <el-tab-pane><template #label><span class="font-size-4">设置</span></template><Settings></Settings></el-tab-pane>
+        <el-tab-pane label="Role"><template #label><span class="font-size-4">关于</span></template><About></About></el-tab-pane>
       </el-tabs>
   </div>
 </template>
---
+++ frontend/src/components/About.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <img src="../assets/images/raw_code.jpg">
+    <div>这是原作者的二维码，请支持原作者</div>
+
+    <div style="margin-top: 1rem;">本项目为开源项目</div>
+    <div>GitHub: https://github.com/RandyWei/harmony-os-tool</div>
+    <div>Gitee: https://gitee.com/RandyWei/harmony-os-tool</div>
+</template>

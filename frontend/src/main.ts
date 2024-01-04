36394e6c3bbd5d06b968e65c60e2209e3073885a
RandyWei <weizhengheng@chinahrt.com>
2024-01-04 16:03:45 +0800

    移除图标库

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,5 @@ import App from './App.vue'
 import './style.css';
 import 'virtual:uno.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 app.mount('#app')
